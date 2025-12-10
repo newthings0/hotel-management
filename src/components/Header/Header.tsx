aeaa5c05acc1ec13578b28b87e805e0aebf52641
newthings0 <felixn.1162@gmail.com>
2025-12-10 15:41:13 -0800

    remove sticky and bg-whtie from header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -36,7 +36,7 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="sticky top-0 z-50 bg-white shadow">
+    <header className=" top-0 z-50 shadow">
       <div className="container mx-auto flex items-center justify-between px-4 py-4">
         <Link href="/" className="hover:text-blue-600">
 
